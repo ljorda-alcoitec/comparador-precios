0808077d0091994c5325e4b17f5680a79b88a152
lucia <ljorda@alcoitec.es>
2018-01-09 13:58:32 +0100

    Initial commit
    Creacion del proyecto

---
+++ ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/LjordaMicroComparadorPreciosApplication.java
@@ -0,0 +1,12 @@
+package es.luciajorda.comparadorprecios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjordaMicroComparadorPreciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjordaMicroComparadorPreciosApplication.class, args);
+	}
+}
