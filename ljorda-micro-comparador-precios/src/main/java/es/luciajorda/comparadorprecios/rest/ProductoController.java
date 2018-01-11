97a85553964a98d0e17720059927228b67d509c7
lucia <ljorda@alcoitec.es>
2018-01-11 12:17:12 +0100

    Creada nueva peticion GET
    Creada nueva peticion que busca el producto por el id  en la base de datos

--- ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/rest/ProductoController.java
+++ ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/rest/ProductoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.luciajorda.comparadorprecios.domain.Producto;
@@ -19,6 +20,11 @@ public class ProductoController {
 	@GetMapping("/producto")
 	public List<Producto> getAll() {
 		return this.productoService.getAllProductos();
+	}
+	
+	@GetMapping("/producto/{id}")
+	public Producto getProductById(@PathVariable("id") Long id) {
+		return this.productoService.getProducto(id);
 	}
 
 }
