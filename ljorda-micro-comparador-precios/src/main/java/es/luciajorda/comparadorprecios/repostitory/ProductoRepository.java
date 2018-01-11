eae11e46e74e2c994c1a7ff4bc29f8ef7e4e1472
lucia <ljorda@alcoitec.es>
2018-01-11 11:26:32 +0100

    Creacion interfaz
    Creada la interfaz ProductoRepositorio que extiende de CrudRepository

---
+++ ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/repostitory/ProductoRepository.java
@@ -0,0 +1,9 @@
+package es.luciajorda.comparadorprecios.repostitory;
+
+import org.springframework.data.repository.CrudRepository;
+
+import es.luciajorda.comparadorprecios.domain.Producto;
+
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
+
+}
