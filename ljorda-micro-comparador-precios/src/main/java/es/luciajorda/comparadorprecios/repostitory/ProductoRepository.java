608d801245d58755cd369c8da638dfff66e00a55
lucia <ljorda@alcoitec.es>
2018-01-11 11:33:13 +0100

    Creacion del servicio  ProductoService
    Creacion de la clase ProductoService que contendra los metodos para crear, modificar, borrar y buscar en la base de datos.
    Añadida a la intefaz la definicion del metodo findProductoByName

--- ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/repostitory/ProductoRepository.java
+++ ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/repostitory/ProductoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import es.luciajorda.comparadorprecios.domain.Producto;
 
 public interface ProductoRepository extends CrudRepository<Producto, Long> {
-
+	Producto findProductoByName(String name);
 }
---
+++ ljorda-micro-comparador-precios/src/main/java/service/ProductoService.java
@@ -0,0 +1,42 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.luciajorda.comparadorprecios.domain.Producto;
+import es.luciajorda.comparadorprecios.repostitory.ProductoRepository;
+
+@Service
+public class ProductoService {
+
+	@Autowired
+	private ProductoRepository productoRepository;
+	
+	public ProductoService() {}
+	public Producto createProducto(Producto producto) {
+		return this.productoRepository.save(producto);
+	}
+	
+	public Producto getProducto(long id) {
+		return this.productoRepository.findOne(id);
+	}
+	
+	public List<Producto> getAllProductos(){
+		return (List<Producto>) this.productoRepository.findAll();
+	}
+	
+	public Producto findProductoByName(String name) {
+		return this.productoRepository.findProductoByName(name);
+	}
+	
+	public void updateProducto(Producto producto) {
+		this.productoRepository.save(producto);
+	}
+	
+	public void deleteProducto(long id) {
+		this.productoRepository.delete(id);
+	}
+	
+}
