5b61b4a8d215ac04f66c808d90eac820a50d1163
lucia <ljorda@alcoitec.es>
2018-01-15 11:35:37 +0100

    DELETE
    Creada la petición para borrar un producto.

--- ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/rest/ProductoController.java
+++ ljorda-micro-comparador-precios/src/main/java/es/luciajorda/comparadorprecios/rest/ProductoController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,11 @@ public class ProductoController {
 		Producto update = this.productoService.getProducto(id);
 		update.setName(producto.getName());
 		this.productoService.createProducto(update);
+	}
+	
+	@DeleteMapping("/producto/{id}")
+	public void deleteProducto(@PathVariable("id") Long id) {
+		this.productoService.deleteProducto(id);
 	}
 
 }
